4f61756ec33e1137140d103dc42fc127f1baa9f1
Samuel Oliveira <sam.oc2005@gmail.com>
2021-11-24 22:21:43 -0300

    arquivos reorganizados

--- LOP/Linguagem C/1- Laços While.c
+++ LOP/Linguagem C/1- Laços While.c
@@ -4,16 +4,18 @@
 
 int i = 0;
 
-int main(void) {
+int main(void)
+{
 
-  while(100>i){
+  while (100 > i)
+  {
     printf("Ciclo: %d\n", i);
     i++;
   }
 
-  while(0<=i){
+  while (0 <= i)
+  {
     printf("Ciclo: %d\n", i);
     i--;
   }
-
 }
