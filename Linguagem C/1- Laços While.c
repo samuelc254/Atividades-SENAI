96ff46654ab83491e49cc149a132349461a37c03
Samuel Oliveira <47918944+samuelc254@users.noreply.github.com>
2021-11-24 22:36:00 -0300

    arquivos reorganizados

---
+++ Linguagem C/1- Laços While.c
@@ -0,0 +1,19 @@
+// teste com lasços de repetição
+
+#include <stdio.h>
+
+int i = 0;
+
+int main(void) {
+
+  while(100>i){
+    printf("Ciclo: %d\n", i);
+    i++;
+  }
+
+  while(0<=i){
+    printf("Ciclo: %d\n", i);
+    i--;
+  }
+
+}
---
+++ Linguagem C/2- Estruturas de comparação If.c
@@ -0,0 +1,29 @@
+//Programa com objetivo de calcular media aritimetica e utilizar condição if
+
+#include <stdio.h>
+
+float n1, n2; 
+float m; // declaração de variavel com valor real
+
+int main(void) {
+
+  while(1){
+    printf("--Programa para calcular média escolar--\n");
+    printf("Insira sua primeira nota: ");
+    scanf("%f",&n1);
+    printf("Insira sua segunda nota: ");
+    scanf("%f",&n2);
+    m = (n1+n2)/2;
+  
+    if (m >= 50)
+    {
+      printf("Sua média é: %.2f\n", m);
+      printf("parabéns você foi aprovado!\n\n");
+    }
+    else
+    {
+      printf("Sua média é: %.2f\n", m);
+      printf("parabéns você foi reprovado!\n\n");
+    }
+  }
+}
